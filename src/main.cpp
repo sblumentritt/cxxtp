14d6be7510c76c73fda2564f6a7f81c2bd91435a
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-02-18 15:58:29 +0100

    Change naming of static meta information variables

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) -> int {
     // print out meta information
-    std::cout << "> Name: " << meta_info::g_project_name << '\n';
-    std::cout << "> Description: " << meta_info::g_project_description << '\n';
+    std::cout << "> Name: " << meta_info::project_name << '\n';
+    std::cout << "> Description: " << meta_info::project_description << '\n';
 
-    std::cout << "> Version: " << meta_info::g_version << '\n';
-    std::cout << "> Revision: " << meta_info::g_version_revision << '\n';
+    std::cout << "> Version: " << meta_info::version << '\n';
+    std::cout << "> Revision: " << meta_info::version_revision << '\n';
 
-    std::cout << "> Build type: " << meta_info::g_build_type << '\n';
-    std::cout << "> Compiler: " << meta_info::g_compiler << '\n';
+    std::cout << "> Build type: " << meta_info::build_type << '\n';
+    std::cout << "> Compiler: " << meta_info::compiler << '\n';
 
     // preferred over return 0
     return EXIT_SUCCESS;
