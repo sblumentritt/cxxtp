3be23ca14e44122b816d61f256ff3566f34742fe
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-07-29 14:14:43 +0200

    Add helper file to initialize a new project (TODO list only)

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# - [ ] get absolute directory where the script is
+# - [ ] add command line parsing
+#     - project name
+#     - target name
+#     - type (executable or library)
+#     - C++ version
+#     - different git username/email
+# - [ ] remove .git folder
+# - [ ] remove .gitmodules file
+# - [ ] remove all folder in dependency/
+# - [ ] git init
+# - [ ] create empty initial commit
+# - [ ] create commit with gitignore
+# - [ ] add 'cmake_modules' submodule to dependency at specific version
+# - [ ] commit submodule addition
+# - [ ] replace project name in CMake
+# - [ ] replace target name in CMake
+# - [ ] depending of the given type uncomment a specific section and remove the other
+# - [ ] commit initial files
+# - [ ] remove placeholder folder?
+# - [ ] remove this script
