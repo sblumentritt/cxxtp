6534f1b1617ea7bc8bc0432e9f895b9215656313
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2021-02-16 11:46:31 +0100

    Add option to allow initialization without Git

--- init.sh
+++ init.sh
@@ -14,6 +14,7 @@ cxx_standard=17
 git_username=
 git_email=
 full_dir_structure=0
+no_git=0
 
 usage() {
     printf "Usage: %s [OPTIONS]
@@ -32,6 +33,8 @@ OPTIONS:
         Default: 17
     -f, --full-dir-structure
         Create all common top-level directories according to the coding guidelines.
+    --no-git
+        No Git repository and commits will be created. '.git/' will no be deleted.
     --git-username <NAME>
         Name which should be used locally in the Git repository to create commits.
     --git-email <ADDRESS>
@@ -59,6 +62,81 @@ check_required_string_option() {
 # find all CMakeLists.txt and call sed with the given string
 find_and_sed() {
     find "${script_dir}" -type f -name CMakeLists.txt -exec sed -i -E "$1" {} \;
+}
+
+create_git_repository_and_commits() {
+    # remove old Git related files/folder
+    rm -rdf "${script_dir}/.git"
+
+    # initialize the Git repo and set local config if requested
+    git init
+
+    printf "\nIs '%s' the correct template dir which should be configured? [y/n] " "${script_dir}"
+    read -r answer
+
+    if [ -n "${git_username}" ]; then
+        git config user.name "${git_username}"
+    fi
+
+    if [ -n "${git_email}" ]; then
+        git config user.email "${git_email}"
+    fi
+
+    # create first commit as empty commit
+    git commit --allow-empty -m "Initial empty commit"
+
+    # add/commit .gitignore file
+    git add "${script_dir}/.gitignore"
+    git commit -m "Add .gitignore file"
+
+    # add 'cmake_modules' as submodule
+    mkdir "${script_dir}/dependency"
+    git submodule add https://github.com/sblumentritt/cmake_modules.git "dependency/cmake_modules"
+
+    cd "${script_dir}/dependency/cmake_modules" || \
+        { printf "Unable to change to '%s'\n" "${script_dir}/dependency/cmake_modules" >&2 ; exit 1; }
+
+    # git checkout "${cmake_modules_tag}"
+
+    cd "${script_dir}" || { printf "Unable to change to '%s'\n" "${script_dir}" >&2; exit 1; }
+
+    git add "${script_dir}/dependency/cmake_modules"
+    git commit -m "Add 'cmake_modules' submodule"
+
+    # commit all relevant files from the template
+    git add "${script_dir}/.clang-format"
+    git add "${script_dir}/.clang-tidy"
+    git add "${script_dir}/CMakeLists.txt"
+    git add "${script_dir}/doc/"
+    git add "${script_dir}/src/"
+
+    git commit -m "Add initial files which come from the 'cxxtp' project template"
+}
+
+generate_and_configure_cmake_files() {
+    if [ "${target_type}" -eq 0 ]; then
+        target_specific_dir="${script_dir}/src/${target_name}"
+
+        mkdir -p "${target_specific_dir}"
+        mv src/CMakeLists.template "${target_specific_dir}/CMakeLists.txt"
+        sed -i -E "s/##TARGET_NAME##/${target_name}/g" "${target_specific_dir}/CMakeLists.txt"
+
+        find_and_sed "s/##SECTION_EXECUTABLE_TYPE##//g"
+        find_and_sed "/##SECTION_LIBRARY_TYPE##/d"
+    else
+        target_specific_dir="${script_dir}/src/lib${target_name}"
+
+        mkdir -p "${target_specific_dir}"
+        mv src/CMakeLists.template "${target_specific_dir}/CMakeLists.txt"
+        sed -i -E "s/##TARGET_NAME##/lib${target_name}/g" "${target_specific_dir}/CMakeLists.txt"
+
+        find_and_sed "s/##SECTION_LIBRARY_TYPE##//g"
+        find_and_sed "/##SECTION_EXECUTABLE_TYPE##/d"
+    fi
+
+    find_and_sed "s/##TEMPLATE_PROJECT_NAME##/${project_name}/g"
+    find_and_sed "s/##TEMPLATE_TARGET_NAME##/${target_name}/g"
+    find_and_sed "s/##TEMPLATE_CXX_STANDARD##/${cxx_standard}/g"
 }
 
 # parse command line arguments
@@ -105,6 +183,9 @@ while :; do
         -f|--full-dir-structure)
             full_dir_structure=1
             ;;
+        --no-git)
+            no_git=1
+            ;;
         --git-username)
             if [ -n "$2" ]; then
                 git_username=$2
@@ -150,74 +231,14 @@ fi
 
 cd "${script_dir}" || { printf "Unable to change to '%s'\n" "${script_dir}" >&2; exit 1; }
 
-# remove old Git related files/folder
-rm -rdf "${script_dir}/.git"
+generate_and_configure_cmake_files
 
-# initialize the Git repo and set local config if requested
-git init
-
-if [ -n "${git_username}" ]; then
-    git config user.name "${git_username}"
-fi
-
-if [ -n "${git_email}" ]; then
-    git config user.email "${git_email}"
-fi
-
-# create first commit as empty commit
-git commit --allow-empty -m "Initial empty commit"
-
-# add/commit .gitignore file
-git add "${script_dir}/.gitignore"
-git commit -m "Add .gitignore file"
-
-# add 'cmake_modules' as submodule
-mkdir "${script_dir}/dependency"
-git submodule add https://github.com/sblumentritt/cmake_modules.git "dependency/cmake_modules"
-
-cd "${script_dir}/dependency/cmake_modules" || \
-    { printf "Unable to change to '%s'\n" "${script_dir}/dependency/cmake_modules" >&2 ; exit 1; }
-
-# git checkout "${cmake_modules_tag}"
-
-cd "${script_dir}" || { printf "Unable to change to '%s'\n" "${script_dir}" >&2; exit 1; }
-
-git add "${script_dir}/dependency/cmake_modules"
-git commit -m "Add 'cmake_modules' submodule"
-
-# replace text placeholder
-if [ "${target_type}" -eq 0 ]; then
-    target_specific_dir="${script_dir}/src/${target_name}"
-
-    mkdir -p "${target_specific_dir}"
-    mv src/CMakeLists.template "${target_specific_dir}/CMakeLists.txt"
-    sed -i -E "s/##TARGET_NAME##/${target_name}/g" "${target_specific_dir}/CMakeLists.txt"
-
-    find_and_sed "s/##SECTION_EXECUTABLE_TYPE##//g"
-    find_and_sed "/##SECTION_LIBRARY_TYPE##/d"
+if [ "${no_git}" -eq 0 ]; then
+    create_git_repository_and_commits
 else
-    target_specific_dir="${script_dir}/src/lib${target_name}"
-
-    mkdir -p "${target_specific_dir}"
-    mv src/CMakeLists.template "${target_specific_dir}/CMakeLists.txt"
-    sed -i -E "s/##TARGET_NAME##/lib${target_name}/g" "${target_specific_dir}/CMakeLists.txt"
-
-    find_and_sed "s/##SECTION_LIBRARY_TYPE##//g"
-    find_and_sed "/##SECTION_EXECUTABLE_TYPE##/d"
+    printf "\nThe template depends on the following Git repositories:\n"
+    printf "    - https://github.com/sblumentritt/cmake_modules %s\n" "${cmake_modules_tag}"
 fi
-
-find_and_sed "s/##TEMPLATE_PROJECT_NAME##/${project_name}/g"
-find_and_sed "s/##TEMPLATE_TARGET_NAME##/${target_name}/g"
-find_and_sed "s/##TEMPLATE_CXX_STANDARD##/${cxx_standard}/g"
-
-# commit all relevant files from the template
-git add "${script_dir}/.clang-format"
-git add "${script_dir}/.clang-tidy"
-git add "${script_dir}/CMakeLists.txt"
-git add "${script_dir}/doc/"
-git add "${script_dir}/src/"
-
-git commit -m "Add initial files which come from the 'cxxtp' project template"
 
 # create common top-level directories if requested
 if [ "${full_dir_structure}" -eq 1 ]; then
